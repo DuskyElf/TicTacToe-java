dc54434bd6e4236deb3a6a83b1f8065c9d96dedb
DuskyElf <clayedison75@gmail.com>
2022-08-25 14:51:07 +0530

    Bug fixed
    Idk why I wrote the bad code in the first place

--- src/Game.java
+++ src/Game.java
@@ -78,25 +78,29 @@ public class Game {
     // Checking if someone won
     Board.Cell winCheck() {
         Board.Cell[][] b = board.boardState;
-        Board.Cell result = Board.Cell.N;
         
         for (int i = 0; i < 3; i++) {
             // Horizontal Checks
+            if (b[i][0] != Board.Cell.N)
             if (b[i][0] == b[i][1] & b[i][1] == b[i][2])
-                result = b[i][0];
+                return b[i][0];
             
             // Virtical Checks
+            if (b[i][0] != Board.Cell.N)
             if (b[0][i] == b[1][i] & b[1][i] == b[2][i])
-                result = b[0][i];
+                return b[0][i];
         }
-
-        // Diagonal Checks
-        if (b[0][0] == b[1][1] & b[1][1] == b[2][2])
-            result = b[0][0];
-        if (b[2][0] == b[1][1] & b[1][1] == b[0][2])
-            result = b[2][0];
         
-        return result;
+        // Diagonal Checks
+        if (b[0][0] != Board.Cell.N)
+        if (b[0][0] == b[1][1] & b[1][1] == b[2][2])
+            return b[0][0];
+
+        if (b[0][0] != Board.Cell.N)
+        if (b[2][0] == b[1][1] & b[1][1] == b[0][2])
+            return b[2][0];
+        
+        return Board.Cell.N;
     }
 
     // Announcment that someone won, and killing the gameloop
